7bb58044db70153640a7987a83d83510ffe35678
nsms556 <nsms556@kakao.com>
2020-10-09 17:51:04 +0900

    delete Unused Line

--- background.py
+++ background.py
@@ -18,29 +18,6 @@ class Background(pygame.sprite.Sprite) :
         if self.currAnimation == BG_IDLE :
             return None
 
-        if self.currAnimation == BG_MOVE_LEFT :
-            self.image, self.rect = load_image('MeetingRoom2.png', 'Background')
-            self.currPosX -= BG_MOVE_SPEED
-            self.rect.topleft = self.currPosX, self.currPosY
-
-        if self.currAnimation == BG_MOVE_RIGHT :
-            self.image, self.rect = load_image('MeetingRoom2.png', 'Background')
-            self.currPosX += BG_MOVE_SPEED
-            self.rect.topleft = self.currPosX, self.currPosY
-
-        if self.currAnimation == BG_MOVE_UP :
-            self.image, self.rect = load_image('MeetingRoom.png', 'Background')
-            self.currPosY -= BG_MOVE_SPEED
-            self.rect.topleft = self.currPosX, self.currPosY
-
-        if self.currAnimation == BG_MOVE_DOWN :
-            self.image, self.rect = load_image('MeetingRoom.png', 'Background')
-            self.currPosY += BG_MOVE_SPEED
-            self.rect.topleft = self.currPosX, self.currPosY
-
-    def updateAnimation(self, animate) :
-       self.currAnimation = animate
-
 class Collision_Line(pygame.sprite.Sprite) :
     def __init__(self, startX, startY, width, height) :
         pygame.sprite.Sprite.__init(self)
